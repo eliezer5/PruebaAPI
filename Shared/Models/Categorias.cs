456aa4fe5571ad3f5eb4552512c8c0395370beb4
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-22 02:12:20 -0400

    AGREGA EXLUDE

--- Shared/Models/Categorias.cs
+++ Shared/Models/Categorias.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.Models;
-
+[ExcludeFromCodeCoverage]
 public class Categorias
 {
     [Key]
--- Shared/Models/ComprasDetalle.cs
+++ Shared/Models/ComprasDetalle.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.Models;
-
+[ExcludeFromCodeCoverage]
 public class ComprasDetalle
 {
     [Key]
