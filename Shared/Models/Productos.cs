587e453c88c1a201d2415787e200d373bd660855
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-22 02:07:43 -0400

    AGREGA EXCLUDE

--- Shared/Models/Productos.cs
+++ Shared/Models/Productos.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.Models;
 
+[ExcludeFromCodeCoverage]
 public class Productos
 {
     [Key]
